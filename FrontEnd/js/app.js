949ce090b73cacf64575c0b4bf6f435fea6c0cb5
JosuaWrw <162317859+JosuaWrw@users.noreply.github.com>
2025-03-24 09:56:34 +0700

    Update app.js

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://localhost:3000/notes")
+  fetch("https://tugas5be-935996462481.us-central1.run.app/notes")
       .then(response => response.json())
       .then(notes => {
           const notesTable = document.getElementById("notesTable");
@@ -34,7 +34,7 @@ function editNote(id) {
 
 function deleteNote(id) {
   if (confirm("Apakah Anda yakin ingin menghapus catatan ini?")) {
-      fetch(`http://localhost:3000/notes/${id}`, { method: "DELETE" })
+      fetch(`https://tugas5be-935996462481.us-central1.run.app/notes/${id}`, { method: "DELETE" })
           .then(() => location.reload())
           .catch(error => console.error("Gagal menghapus catatan:", error));
   }
